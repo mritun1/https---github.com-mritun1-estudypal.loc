301b8420a9476707ffd42c68cf75dc2f5109be8a
mritun1 <103233905+mritun1@users.noreply.github.com>
2022-06-03 15:53:07 +0530

    change on api

--- api/api/art.php
+++
@@ -1,1 +0,0 @@
-<?php StartAPI('ART', $exp_req[3], $exp_req[4]); ?>
--- api/api/users.php
+++ api/api/users.php
@@ -1,1 +1,4 @@
-<?php StartAPI('USERS', $exp_req[3], $exp_req[4]); ?>
+<?php 
+header('Access-Control-Allow-Origin: *');
+StartAPI('USERS', $exp_req[3], $exp_req[4]); 
+?>
